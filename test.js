3f491a2537ebfeedda159a607ea90c666ae0da5a
Kesha <djychum@mail.ru>
2016-08-11 23:11:14 +0700

    test

--- test.js
+++ test.js
@@ -1,3 +1,4 @@
-<script>
-alert("XSS")
-</script>
+<img src="http://url.to.file.which/not.exist" onerror=alert(document.cookie);>
+<IMG SRC=j&#X41vascript:alert('test2')>
+<META HTTP-EQUIV="refresh"
+CONTENT="0;url=data:text/html;base64,PHNjcmlwdD5hbGVydCgndGVzdDMnKTwvc2NyaXB0Pg">
